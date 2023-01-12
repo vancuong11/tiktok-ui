0780cbb8ecb500b9d09721bc583b485ff8d15501
vancuong11 <cuongg121101@gmail.com>
2023-01-12 11:09:54 +0700

    Bài 95: Cấu hình Router/Layout cho dự án

--- knowledge.md
+++ knowledge.md
@@ -9,3 +9,8 @@
    2 Cài thư viện SASS: npm i -D sass
    3 Reset CSS: npm install --save normalize.css
    4 Default CSS: font-family, font-size, line-height
+6. Cấu hình Router/Layout cho dự án:
+   1 Phân tích tổng quan Layout
+   2 Cài đặt react-router-dom: npm i react-router-dom
+   3 Đưa cấu hình routes ra ngoài
+   4 Xây dựng cơ chế tải Layout
--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import DefaultLayout from '~/components/Layout/DefaultLayout';
+import { Fragment } from 'react';
+
 function App() {
     return (
-        <div className="App">
-            <h1>React App</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        const Page = route.component;
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
