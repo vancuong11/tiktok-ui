2b506cbf15cf631f223bcde73db05c6f6006fe69
vancuong11 <cuongg121101@gmail.com>
2023-02-01 21:40:57 +0700

    Bài 114: Xây dựng UI phần sidebar #1

--- src/Layouts/MaintLayout/Sidebar/Sidebar.js
+++ src/Layouts/MaintLayout/Sidebar/Sidebar.js
@@ -1,12 +1,33 @@
 import styles from './Sidebar.module.scss';
 import classNames from 'classnames/bind';
 
+import Menu, { MenuItem } from './Menu';
+import config from '~/config';
+
+import {
+    HomeIcon,
+    HomeActiveIcon,
+    UserGroupIcon,
+    UserGroupActiveIcon,
+    LiveIcon,
+    LiveActiveIcon,
+} from '~/components/Icons';
+
 const cx = classNames.bind(styles);
 
 function Sidebar() {
     return (
         <aside className={cx('wrapper')}>
-            <h2>Sidebar</h2>
+            <Menu>
+                <MenuItem title="For You" to={config.routes.home} icon={<HomeIcon />} activeIcon={<HomeActiveIcon />} />
+                <MenuItem
+                    title="Following"
+                    to={config.routes.following}
+                    icon={<UserGroupIcon />}
+                    activeIcon={<UserGroupActiveIcon />}
+                />
+                <MenuItem title="Live" to={config.routes.live} icon={<LiveIcon />} activeIcon={<LiveActiveIcon />} />
+            </Menu>
         </aside>
     );
 }
